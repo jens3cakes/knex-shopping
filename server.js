0a84061471260decdbda0dfe14f1dcdf20d363cc
Jennifer Hikichi <jenniferhikichi@Jennifers-MBP.home>
2018-11-03 17:45:33 -1000

    files download

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+const usersRouter = require('./routes/users');
+const productsRoouter = require('./routes/products');
+const cartRouter = require('./routes/cart'); 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+app.get('/', (req,res)=>{
+  res.send('smoke test')
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, (req, res)=>{
+  console.log(`Server is listening on ${PORT}`);
+})
+
